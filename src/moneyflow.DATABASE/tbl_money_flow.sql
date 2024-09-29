89a33f7aa589a1069378911fbdc2829967fa6dc6
Eduardo Mesquita <kadubezas@gmail.com>
2024-09-29 12:18:28 -0300

    :tada: commit inicial

---
+++ src/moneyflow.DATABASE/tbl_money_flow.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[tbl_money_flow]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ src/moneyflow.TEST/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace moneyflow.TEST
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
